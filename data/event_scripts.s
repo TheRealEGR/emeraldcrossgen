48090ee872b69a2431ce85ba6970d60bf494db9f
TheRealEGR <razvanrazvan502@gmail.com>
2024-09-28 19:59:51 +0200

    1

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1056,18 +1056,3 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
-
-FillPokedex::
-	setvar VAR_0x8004, 1
-	special ScriptSetSeenPokedexInfo
-	special ScriptSetCaughtPokedexInfo
-	call loop 
-	setflag FLAG_SYS_POKEDEX_GET
-	special EnableNationalPokedex
-loop:
-	addvar Ox8004 1
-	special ScriptSetSeenPokedexInfo
-	special ScriptSetCaughtPokedexInfo
-	compare VAR_0x8004, 386
-	goto_if_lt loop 
-	return
